56d9e7a7ef0e4d32d7373ab770ddb684c412449e
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-10-27 17:15:36 -0700

    Added play again button and more styles

--- endgame/endgame.js
+++ endgame/endgame.js
@@ -1,3 +1,17 @@
 import { userProfile, getUser } from '../utils.js';
-getUser();
+const user = getUser();
 userProfile();
+
+const button = document.getElementById('btn');
+const resultMessage = document.getElementById('result-message');
+
+button.addEventListener('click', ()=>{
+    localStorage.clear(userProfile);
+    window.location.replace('../');
+});
+
+if (user.hp <= 0){
+    resultMessage.textContent = 'Your adventure ends here. Unfortunatley ends here the seas have taken you away.';
+} else {
+    resultMessage.textContent = 'Awesome! You are a very powerful pirate. The king of pirates is in reach!';
+}
